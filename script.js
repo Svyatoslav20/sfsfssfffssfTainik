030c9dfd7e9af9e2dd342e9b13f343dc84e13a47
Svyatoslav20 <m68405990@gmail.com>
2026-01-03 19:31:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-let btnStart = document.querySelector(".btn__start")
-let btnEnd = document.querySelector(".btn__delete")
-let btnToggle = document.querySelector(".togglebtn")
-let noneBtn = document.querySelector('#none');
-// Кнопки
+const btnStart = document.querySelector(".btn__start")
+const btnEnd = document.querySelector(".btn__delete")
+const btnToggle = document.querySelector(".togglebtn")
+const noneBtn = document.querySelector('#none');
+const openBtn = document.querySelector('.open')
+// Кнопки 
+const svgStart = document.querySelector(".send")
+const svgEnd = document.querySelector(".del")
+const instruction = document.querySelector('.instruction')
+// Доп. констанция 
 
 let firstInput = document.querySelector(".first")
 let secondInput = document.querySelector(".second")
 let thirdInput = document.querySelector(".third")
 let basement = document.querySelector(".bas")
+let tempDiv = document.querySelector('.tempDiv')
 let tempNew = document.querySelector('#template').cloneNode(true).content;
 function numB(event) {
+    event.preventDefault()
     let frt = firstInput.value 
     let scd = secondInput.value 
     let thd = thirdInput.value 
-    event.preventDefault()
+    let userProV = document.querySelector('.deinput')
     let result = parseInt(frt) + parseInt(scd) + parseInt(thd)
-    let userRes = Math.floor(result /3)
+    let userRes = result / userProV.value 
+    if (userRes !== Math.floor(userRes)) {          // Заметка самому себе: "!==" - НЕ РАВНО. Слон (a) не равен(!==) ежу (b) = a !== b
+        userRes = userRes.toFixed(2)
+    }
     let tempNew = document.querySelector('#template').cloneNode(true).
     content;
     let p = tempNew.querySelector(".list")
     if(frt === "" || scd === "" || thd === "") {
-        p.textContent = "Вы, вероятно, не вписали число в одну из строк..."
+        p.textContent = "Вы не вписали число в одну из строк."
     }
     else {
-        p.textContent = "Успех! Вот оно: " + userRes
+        p.textContent = "Успех! Вот ваш сюрприз: " + userRes
+    }
+    
+    if (Number(userProV.value === 0)) {
+        p.textContent = "Измените значение."
     }
     btnEnd.addEventListener('click', delnumB)
-    basement.append(tempNew)
-
+    tempDiv.append(tempNew)
+    
 }
 btnStart.addEventListener('click', numB)
 function delnumB(event) {
@@ -37,7 +51,46 @@ function delnumB(event) {
 }
 btnEnd.addEventListener('click', delnumB)
 // Функции очистки и расчета
+
 btnToggle.onclick = () => {
     noneBtn.style.display = 'block'
 }
 noneBtn.style.display = 'none'
+// Функция кнопки ВКЛЮЧИТЬ
+
+const deMenu = document.querySelector('.debugMenu')
+let debugOpen = false;
+function opDebug(event) {
+    event.preventDefault()
+    console.log('Консоль открыта!')
+
+    if (!debugOpen) {
+        // гарантируем стартовое состояние
+        deMenu.classList.remove('active');
+        // следующий кадр - ВКЛ
+        requestAnimationFrame(() => {
+            deMenu.classList.add('active');
+        });
+    } else {
+        deMenu.classList.remove('active');
+    }
+
+    debugOpen = !debugOpen;
+}
+openBtn.addEventListener('click', opDebug)
+// функция включения и выкл. менюшки
+
+function delInst(event) {
+    event.preventDefault()
+    instruction.remove()
+}
+instruction.addEventListener('click', delInst)
+// Функция удаления инструкции
+let deinput = document.querySelector('.deinput')
+const minval = 1 ;
+const maxval = 9999;
+deinput.addEventListener('input', () =>{
+    let val = Number(deinput.value)
+    if (val > maxval) deinput.value = maxval      // отбираем возможность бесконечного числа
+    if(val < minval) deinput.value = minval
+})
